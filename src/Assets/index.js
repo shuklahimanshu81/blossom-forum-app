680d8919d5fbf4bdc5b8031bbbb8dfb6ae8d9e1a
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-09-19 22:34:05 +0530

    chore : Assets added

---
+++ src/Assets/index.js
@@ -0,0 +1,25 @@
+import homeVector from "./Home-Vector.png";
+import exploreVector from "./Explore-Vector.png";
+import notificationVector from "./Notification-Vector.png";
+import profileVector from "./Profile-vector.png";
+import bookmarksVector from "./Bookmarks-Vector.png";
+import commentVector from "./comment-vector.png";
+import postBookmark from "./post-bookmark-vector.png";
+import shareVector from "./share-vector.png";
+import threeDots from "./three-dots.png";
+import downVoteBtn from "./down-vote-vector.png";
+import upVoteBtn from "./upvote-vector.png";
+
+export {
+  homeVector,
+  exploreVector,
+  notificationVector,
+  profileVector,
+  bookmarksVector,
+  commentVector,
+  postBookmark,
+  shareVector,
+  threeDots,
+  downVoteBtn,
+  upVoteBtn,
+};
